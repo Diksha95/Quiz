bfd3f95b11ca5e7dd66c4e91d763eb273856a4ee
Diksha <sharma.diksha2595@gmail.com>
2017-07-15 20:48:49 +0530

    second commit

--- app/src/main/java/com/example/diksha/quiz/MainActivity.java
+++ app/src/main/java/com/example/diksha/quiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diksha.quiz;
 import android.R.*;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +13,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
+    TextView tv,tv2;
     RadioGroup radioGroup;
     RadioButton radioButton;
     Button btnnext;
-    int id,j;
+    int id;
+    int count=0;
     Questions q1,q2,q3,q4,q;
     ArrayList<Questions> list;
     @Override
@@ -26,35 +28,19 @@ public class MainActivity extends AppCompatActivity {
         tv=(TextView)findViewById(R.id.ques);
         radioGroup=(RadioGroup)findViewById(R.id.rg1);
         btnnext=(Button)findViewById(R.id.button);
+        tv2=(TextView)findViewById(R.id.timer);
         id=radioGroup.getCheckedRadioButtonId();
         radioButton=(RadioButton)findViewById(id);
-        q1=new Questions();
-        q1.setQues("Ques 1.What is the full form of SMTP?");
-        q1.setOption1("Simple Mail Transfer Protocol");
-        q1.setOption2("Simple Mail Text Person");
-        q1.setOption3("Simple Message Transfer Protocol");
-        q1.setOption4("Sound Mail Text Pattern");
+        q1=new Questions("Ques 1.What is the full form of SMTP?","Simple Mail Transfer Protocol",
+                "Simple Mail Text Person","Simple Message Transfer Protocol","Sound Mail Text Pattern");
 
-        q2=new Questions();
-        q2.setQues("Ques 2.What is the full form of TCP?");
-        q2.setOption1("Transfer Control Protocol");
-        q2.setOption2("Text Control Protocol");
-        q2.setOption3("Transfer Communication Person");
-        q2.setOption4("Text Control Person");
+        q2=new Questions("Ques 2.What is the full form of TCP?","Transfer Control Protocol",
+                "Text Control Protocol","Transfer Communication Person","Text Control Person");
 
-        q3=new Questions();
-        q3.setQues("Ques 3.What DNS stands for?");
-        q3.setOption1("Domain Name System");
-        q3.setOption2("Domain Name Server");
-        q3.setOption3("Distributed Name Server");
-        q3.setOption4("Distributed Name System");
+        q3=new Questions("Ques 3.What DNS stands for?","Domain Name System","Domain Name Server","Distributed Name Server",
+                "Distributed Name System");
 
-        q4=new Questions();
-        q4.setQues("Ques 4.What is a port?");
-        q4.setOption1("Numbered Socket");
-        q4.setOption2("Protocol");
-        q4.setOption3("Server");
-        q4.setOption4("Path");
+        q4=new Questions("Ques 4.What is a port?","Numbered Socket","Protocol","Server","Path");
         list=new ArrayList<>();
         list.add(q1);
         list.add(q2);
@@ -67,25 +53,57 @@ public class MainActivity extends AppCompatActivity {
         ((RadioButton) radioGroup.getChildAt(1)).setText(q1.getOption2());
         ((RadioButton) radioGroup.getChildAt(2)).setText(q1.getOption3());
         ((RadioButton) radioGroup.getChildAt(3)).setText(q1.getOption4());
-        String Ques[]=new String[4];
-
-       /* for(int i=0;i<list.size();i++)
-        {
-            Questions q=list.get(i);
-            tv.setText(q.getQues());
-        }*/
-       btnnext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(++j >= list.size()){
-                    j = 0;
-                    q=list.get(j);
-                    tv.setText(q.getQues());
+        startTimer(60,tv2);
+            btnnext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(count<=list.size())
+                    {
+                        count++;
+                        setQuestion();
+                    }
                 }
+            });
+        }
 
+    public void setQuestion(){
+        tv.setText(list.get(count).getQues());
+        ((RadioButton) radioGroup.getChildAt(0)).setText(list.get(count).getOption1());
+        ((RadioButton) radioGroup.getChildAt(1)).setText(list.get(count).getOption2());
+        ((RadioButton) radioGroup.getChildAt(2)).setText(list.get(count).getOption3());
+        ((RadioButton) radioGroup.getChildAt(3)).setText(list.get(count).getOption4());
+    }
+    //Declare timer
+    CountDownTimer cTimer = null;
+    //start timer function
+    void startTimer(int Seconds,final TextView tv) {
+        cTimer = new CountDownTimer(Seconds* 1000+1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+                tv.setText("TIME : " + String.format("%02d", minutes)
+                        + ":" + String.format("%02d", seconds));
             }
-        });
+            public void onFinish() {
+                tv2.setText("Times Up!");
+            }
+        };
+        cTimer.start();
+    }
+
+    //cancel timer
+    void cancelTimer() {
+        if(cTimer!=null)
+            cTimer.cancel();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cTimer.cancel();
+    }
+
 
     }
-}
+
--- app/src/main/java/com/example/diksha/quiz/Questions.java
+++ app/src/main/java/com/example/diksha/quiz/Questions.java
@@ -11,6 +11,14 @@ public class Questions {
     private String option3;
     private String option4;
 
+    public Questions(String ques, String option1, String option2, String option3, String option4) {
+        this.ques = ques;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+    }
+
     public String getQues() {
         return ques;
     }
